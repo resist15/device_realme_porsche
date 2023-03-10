a4b340e2a4eacf5fee51e85d603554749adeedca
Kunmun <kunmun@aospa.co>
2023-03-10 20:00:23 +0530

    porsche: Move manifest files into configs/vintf
    Change-Id: Id68479bbfcd1004fc0de76ee957898f77046bfbf

--- device.mk
+++ device.mk
@@ -111,10 +111,10 @@ PRODUCT_PACKAGES += \
    android.hardware.keymaster@4.1.vendor
 
 # Manifests
-DEVICE_MANIFEST_FILE := device/realme/porsche/manifest_lahaina.xml
+DEVICE_MANIFEST_FILE := device/realme/porsche/configs/vintf/manifest_lahaina.xml
 DEVICE_MATRIX_FILE := device/qcom/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(DEVICE_PATH)/porsche_vendor_framework_compatibility_matrix.xml
+    $(DEVICE_PATH)/configs/vintf/porsche_vendor_framework_compatibility_matrix.xml
 
 # Namespaces
 PRODUCT_SOONG_NAMESPACES += \
