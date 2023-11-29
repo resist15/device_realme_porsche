c723996449255716ec8cd2611198c2098c0dabc7
Omkar Chandorkar <gotenksIN@aospa.co>
2023-11-29 23:15:39 +0900

    porsche: overlay: Enable Wifi overlay for mainline modules
    Change-Id: Iac8be06e58d24d868a33d212d80a583bad8705ef
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- device.mk
+++ device.mk
@@ -181,7 +181,8 @@ PRODUCT_PACKAGES += \
     PorscheSettings \
     PorscheSettingsProvider \
     PorscheSystemUI \
-    PorscheWifiResTarget
+    PorscheWifiResTarget \
+    PorscheWifiResTargetMainline
 
 # Partitions - Dynamic
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
